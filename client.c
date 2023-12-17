506490f989807f1ed9bf8128f59ceffec7a6bec0
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-12-18 01:34:30 +0900

    Docs : 주석 추가

--- client.c
+++ client.c
@@ -15,38 +15,49 @@ void start_client()
   char *http_request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
   char buffer[1024] = {0};
 
+  // 소켓 생성
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     printf("\n Socket creation error \n");
     return;
   }
 
+  // 서버 주소 설정
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
-  // Convert IPv4 and IPv6 addresses from text to binary form
+  // IPv4 주소를 이진 형식으로 변환하여 설정
   if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
   {
     printf("\nInvalid address/ Address not supported \n");
     return;
   }
 
+  // 서버에 연결
   if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
   {
     printf("\nConnection Failed \n");
     return;
   }
 
+  // HTTP 요청을 서버에 보냄
   send(sock, http_request, strlen(http_request), 0);
   printf("HTTP request sent\n");
+
+  // 서버로부터 응답을 읽어와 출력
   read(sock, buffer, 1024);
   printf("%s\n", buffer);
 
+  // 소켓 닫기
   close(sock);
 }
 
 int main()
 {
-  start_client();
+  start_client(); // 클라이언트 시작
   return 0;
 }
+
+// 이 코드는 간단한 HTTP 클라이언트를 구현한 것으로, 지정된 서버에 HTTP
+// GET 요청을 보내고 응답을 수신하여 출력합니다.코드 내에 주석을 추가하
+// 여 각 부분의 동작과 역할을 상세히 설명하였습니다.
--- server.c
+++ server.c
@@ -16,59 +16,65 @@ void start_server()
   char buffer[1024] = {0};
   char *json_response = "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{\"message\": \"Hello, world!\"}";
 
-  // Creating socket file descriptor
+  // 소켓 파일 디스크립터를 생성
   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
   {
-    perror("socket failed");
+    perror("socket failed"); // 소켓 생성 실패 시 오류 메시지 출력
     exit(EXIT_FAILURE);
   }
 
-  // Forcefully attaching socket to the port 8080
+  // 포트 재사용 옵션 설정 (SO_REUSEADDR 및 SO_REUSEPORT)
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
   {
-    perror("setsockopt");
+    perror("setsockopt"); // 소켓 옵션 설정 실패 시 오류 메시지 출력
     exit(EXIT_FAILURE);
   }
 
+  // 서버 주소 설정
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
 
-  // Binding the socket to the network address and port
+  // 소켓을 네트워크 주소와 포트에 바인딩
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
   {
-    perror("bind failed");
+    perror("bind failed"); // 바인딩 실패 시 오류 메시지 출력
     exit(EXIT_FAILURE);
   }
 
-  // Listening for incoming connections
+  // 연결 요청을 수신하기 위해 소켓을 대기 상태로 설정
   if (listen(server_fd, 3) < 0)
   {
-    perror("listen");
+    perror("listen"); // 대기 모드 설정 실패 시 오류 메시지 출력
     exit(EXIT_FAILURE);
   }
 
-  // Accepting an incoming connection
+  // 클라이언트의 연결을 수락
   if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
   {
-    perror("accept");
+    perror("accept"); // 연결 수락 실패 시 오류 메시지 출력
     exit(EXIT_FAILURE);
   }
 
-  // Reading request from the socket
+  // 소켓으로부터 요청 데이터 읽기
   read(new_socket, buffer, 1024);
   printf("%s\n", buffer);
 
-  // Sending JSON response
+  // JSON 응답 보내기
   send(new_socket, json_response, strlen(json_response), 0);
   printf("JSON response sent\n");
 
+  // 소켓 및 서버 소켓 닫기
   close(new_socket);
   close(server_fd);
 }
 
 int main()
 {
-  start_server();
+  start_server(); // 서버 시작
   return 0;
 }
+
+// 이 코드는 간단한 HTTP 서버를 구현한 것으로,
+// 요청을 받으면 "Hello, world!"와 같은 JSON 응답을 클라이언트에게 보내는 역할을 합니다.
+// 코드 내에 주석을 추가하여 각 부분의 동작과 역할을 상세히 설명하였습니다.
